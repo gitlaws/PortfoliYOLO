4eea4056dbaca20345730a498405da2dbc68505a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-17 01:13:40 -0400

    Add Theme enum

---
+++ src/app/shared/services/theme/theme.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Theme } from '../../models/theme.enum';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private themeKey = 'theme';
+  private themeSubject: BehaviorSubject<Theme>;
+
+  constructor(private storageService: StorageService) {
+    const storedTheme =
+      (this.storageService.getItem(this.themeKey) as Theme) || Theme.Light;
+    this.themeSubject = new BehaviorSubject<Theme>(storedTheme);
+  }
+
+  get currentTheme() {
+    return this.themeSubject.asObservable();
+  }
+
+  toggleTheme(): void {
+    const newTheme =
+      this.themeSubject.value === Theme.Light ? Theme.Dark : Theme.Light;
+    this.storageService.setItem(this.themeKey, newTheme);
+    this.themeSubject.next(newTheme);
+  }
+}
