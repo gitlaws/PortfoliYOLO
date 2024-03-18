d8fdb74ceed967613ea6db3a39fd26f23265466a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-18 00:11:41 -0400

    Refactor theme-toggle component HTML and SCSS files

--- src/app/shared/common/theme-toggle/theme-toggle.component.ts
+++ src/app/shared/common/theme-toggle/theme-toggle.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../../shared/services/theme/theme.service';
+import { Theme } from '../../../shared/models/theme.enum';
 
 @Component({
   selector: 'app-theme-toggle',
-  standalone: true,
-  imports: [],
   templateUrl: './theme-toggle.component.html',
-  styleUrl: './theme-toggle.component.scss'
+  styleUrl: './theme-toggle.component.scss',
 })
 export class ThemeToggleComponent {
+  isDarkMode!: boolean;
+  isAnimated: boolean = false;
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.isDarkMode = theme === Theme.Dark;
+    });
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+    this.isAnimated = true;
+    setTimeout(() => {
+      this.isAnimated = false;
+    }, 500); // Remove the class after the duration of the animation
+  }
+
+  toggleLogo() {
+    const logo = document.getElementById('logo') as HTMLImageElement;
+    logo.src = logo.src.includes('logo78px')
+      ? 'assets/logo78px.png'
+      : 'assets/logo78px.png';
+  }
 }
