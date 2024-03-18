15103e8b92221c3078972a8faddc1bc897b5dc6d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-18 01:55:27 -0400

    Add social links and theme toggle modules to app module, update toolbar component HTML and SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './core/toolbar/toolbar.component';
 import { MainNavModule } from './core/main-nav/main-nav.module';
+import { SocialLinksModule } from './shared/common/social-links/social-links.module';
+import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
 
 @NgModule({
   declarations: [ToolbarComponent],
   exports: [ToolbarComponent],
-  imports: [CommonModule, MainNavModule],
+  imports: [CommonModule, MainNavModule, SocialLinksModule, ThemeToggleModule],
 })
 export class AppModule {}
