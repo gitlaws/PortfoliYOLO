6143454323e0fd52b81b5d06cb0813000abbf093
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-19 00:05:52 -0400

    Remove toolbar component from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ToolbarComponent } from './core/toolbar/toolbar.component';
 import { SocialLinksModule } from './shared/common/social-links/social-links.module';
 import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
 import { ToolbarMenuModule } from './core/toolbar-menu/toolbar-menu.module';
 import { LinksModule } from './shared/common/links/links.module';
 
 @NgModule({
-  declarations: [ToolbarComponent],
-  exports: [ToolbarComponent],
+  exports: [],
   imports: [
     CommonModule,
     SocialLinksModule,
--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
-
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
