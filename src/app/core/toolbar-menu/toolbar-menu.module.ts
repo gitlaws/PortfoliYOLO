20ab12f1c1484543f380b399098f9594a51e97fe
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-18 23:55:30 -0400

    Add app-links component to toolbar-menu.component.html and correct path in toolbar-menu.module.ts

--- src/app/core/toolbar-menu/toolbar-menu.module.ts
+++ src/app/core/toolbar-menu/toolbar-menu.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LinksModule } from '../shared/common/links/links.module'; // corrected path
 import { ToolbarMenuComponent } from './toolbar-menu.component';
 
 @NgModule({
   declarations: [ToolbarMenuComponent],
   exports: [ToolbarMenuComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LinksModule],
 })
 export class ToolbarMenuModule {}
