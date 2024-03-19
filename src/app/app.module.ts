e11de7b18876d38804ecc8bf293c2d14fb4b78c9
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-19 00:12:44 -0400

    Reorder imports and add ToolbarModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SocialLinksModule } from './shared/common/social-links/social-links.module';
-import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
 import { ToolbarMenuModule } from './core/toolbar-menu/toolbar-menu.module';
+import { ToolbarModule } from './core/toolbar/toolbar.module';
 import { LinksModule } from './shared/common/links/links.module';
+import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
+import { SocialLinksModule } from './shared/common/social-links/social-links.module';
 
 @NgModule({
-  exports: [],
   imports: [
     CommonModule,
-    SocialLinksModule,
-    ThemeToggleModule,
-    ToolbarMenuModule,
+    ToolbarModule,
     LinksModule,
+    ThemeToggleModule,
+    SocialLinksModule,
+    ToolbarMenuModule,
   ],
 })
 export class AppModule {}
