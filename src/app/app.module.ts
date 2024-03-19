543c2e473521bba5deb26b69877b070341da9f15
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-19 01:28:25 -0400

    Add toolbar component and reorganize module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ToolbarMenuModule } from './core/toolbar-menu/toolbar-menu.module';
-import { ToolbarModule } from './core/toolbar/toolbar.module';
-import { LinksModule } from './shared/common/links/links.module';
-import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
+import { ToolbarComponent } from './core/toolbar/toolbar.component';
 import { SocialLinksModule } from './shared/common/social-links/social-links.module';
+import { ThemeToggleModule } from './shared/common/theme-toggle/theme-toggle.module';
+import { ToolbarMenuModule } from './core/toolbar-menu/toolbar-menu.module';
+import { LinksModule } from './shared/common/links/links.module';
 
 @NgModule({
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent],
   imports: [
     CommonModule,
-    ToolbarModule,
-    LinksModule,
-    ThemeToggleModule,
     SocialLinksModule,
+    ThemeToggleModule,
     ToolbarMenuModule,
+    LinksModule,
   ],
 })
 export class AppModule {}
