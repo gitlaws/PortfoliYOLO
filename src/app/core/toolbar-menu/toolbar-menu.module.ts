4be68aa9661817c801f36c437b8eef662ae1bc05
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-20 17:33:34 -0400

    Add menu links to toolbar menu component

--- src/app/core/toolbar-menu/toolbar-menu.module.ts
+++ src/app/core/toolbar-menu/toolbar-menu.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarMenuComponent } from './toolbar-menu.component';
+import { ThemeToggleModule } from '../../shared/common/theme-toggle/theme-toggle.module';
+import { SocialLinksModule } from '../../shared/common/social-links/social-links.module';
 
 @NgModule({
   declarations: [ToolbarMenuComponent],
   exports: [ToolbarMenuComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ThemeToggleModule, SocialLinksModule],
 })
 export class ToolbarMenuModule {}
