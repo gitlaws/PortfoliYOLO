29afec93046def5f5cb795937e2032e130edc1b3
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-21 17:51:43 -0400

    Add tab property to ProfileComponent

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  tab: any;
+}
