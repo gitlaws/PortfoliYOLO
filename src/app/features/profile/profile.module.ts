f304f6d1c82846111a5475474be832d09f2cfd9f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-21 18:14:31 -0400

    Remove unused tab and tabnav components

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ProfileTabnavComponent } from './profile-tabnav/profile-tabnav.component';
-import { TabComponent } from './tab/tab.component';
-import { TabnavModule } from './tabnav/tabnav.module';
 
 @NgModule({
   declarations: [ProfileComponent],
   exports: [ProfileComponent],
-  imports: [CommonModule, TabComponent, TabnavModule],
+  imports: [CommonModule, ProfileTabnavComponent],
 })
 export class ProfileModule {}
--- src/app/features/profile/tabnav/tabnav.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TabnavComponent } from './tabnav.component';
-
-@NgModule({
-  imports: [CommonModule, TabnavComponent],
-  exports: [TabnavComponent],
-})
-export class TabnavModule {}
