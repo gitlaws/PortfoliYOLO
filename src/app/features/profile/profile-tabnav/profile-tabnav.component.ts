d87ddba8e4a780d286cfcdbff7416cb89214330a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-21 05:28:47 -0400

    Add tab navigation component to profile page

--- src/app/features/profile/profile-tabnav/profile-tabnav.component.ts
+++ src/app/features/profile/profile-tabnav/profile-tabnav.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-profile-tabnav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-tabnav.component.html',
   styleUrl: './profile-tabnav.component.scss',
 })
-export class ProfileTabnavComponent {}
+export class ProfileTabnavComponent {
+  activeTab: any;
+}
