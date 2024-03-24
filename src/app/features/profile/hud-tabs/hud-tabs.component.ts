346f47b7a412b1f3039ce948daa4374cee23958c
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-23 22:22:40 -0400

    Add tab navigation and pagination to HUD tabs component

--- src/app/features/profile/hud-tabs/hud-tabs.component.ts
+++ src/app/features/profile/hud-tabs/hud-tabs.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hud-tabs.component.html',
-  styleUrl: './hud-tabs.component.scss'
+  styleUrl: './hud-tabs.component.scss',
 })
 export class HudTabsComponent {
+  activeTab: any;
+  activeSlide = 1;
+  currentPage: any;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.activeTab = 'tab1';
+    this.currentPage = 1; // Initialize currentPage to 1
+  }
+
+  selectSlide(slideNumber: number) {
+    this.activeSlide = slideNumber;
+    // Add your logic here to change the slide content
+  }
 }
