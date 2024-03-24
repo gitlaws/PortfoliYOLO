45fb35b1f9d4d670fe802a78fae4263bee4d2ea3
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-23 22:01:43 -0400

    Add profile component and update module imports

---
+++ src/app/features/profile/hud-tabs/hud-tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hud-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './hud-tabs.component.html',
+  styleUrl: './hud-tabs.component.scss'
+})
+export class HudTabsComponent {
+
+}
--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ProfileTabnavComponent } from './profile-tabnav/profile-tabnav.component';
+import { HudTabsComponent } from './hud-tabs/hud-tabs.component';
 
 @NgModule({
   declarations: [ProfileComponent],
   exports: [ProfileComponent],
-  imports: [CommonModule, ProfileTabnavComponent],
+  imports: [CommonModule, ProfileTabnavComponent, HudTabsComponent],
 })
 export class ProfileModule {}
