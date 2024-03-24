211f83b0ccffef3edf9fb77721c9dfb2385872f5
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-03-23 23:37:09 -0400

    Remove unused HUD tabs component and update profile component

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { ProfileTabnavComponent } from './profile-tabnav/profile-tabnav.component';
-import { HudTabsComponent } from './hud-tabs/hud-tabs.component';
 
 @NgModule({
   declarations: [ProfileComponent],
   exports: [ProfileComponent],
-  imports: [CommonModule, ProfileTabnavComponent, HudTabsComponent],
+  imports: [CommonModule, ProfileTabnavComponent],
 })
 export class ProfileModule {}
