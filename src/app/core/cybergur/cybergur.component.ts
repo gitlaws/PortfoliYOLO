7fef914006c481d629c9cf5f27fc095048c969d8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-01 01:03:04 -0400

    Update function to handle edge cases

---
+++ src/app/core/cybergur/cybergur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cybergur',
+  standalone: true,
+  imports: [],
+  templateUrl: './cybergur.component.html',
+  styleUrl: './cybergur.component.scss'
+})
+export class CybergurComponent {
+
+}
