92e52633fc72b4861369f53cfc5061ca1ee23b4d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-16 19:37:18 -0400

    Add projects component and update related files

--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './projects.component';
 
 const routes: Routes = [{ path: '', component: ProjectsComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  declarations: [ProjectsComponent], // Declare ProjectsComponent here
+  imports: [
+    CommonModule, // Include CommonModule in the imports array
+    RouterModule.forChild(routes),
+  ],
   exports: [RouterModule],
 })
 export class ProjectsModule {}
