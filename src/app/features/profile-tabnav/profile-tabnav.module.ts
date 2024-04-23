a5746e6ee62d459c2ae310518759891a1e4ecaae
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-23 17:47:30 -0400

    Update routing configuration and adjust profile tabnav colors

--- src/app/features/profile-tabnav/profile-tabnav.module.ts
+++ src/app/features/profile-tabnav/profile-tabnav.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProfileTabnavComponent } from './profile-tabnav.component'; // Import ProfileTabnavComponent
+import { ProfileTabnavComponent } from './profile-tabnav.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'profile-tabnav', component: ProfileTabnavComponent },
+];
 
 @NgModule({
-  declarations: [
-    ProfileTabnavComponent, // Declare ProfileTabnavComponent
-    // other declarations...
-  ],
-  exports: [
-    ProfileTabnavComponent, // Export ProfileTabnavComponent if it's used in other modules
-  ],
-  imports: [
-    CommonModule,
-    // other imports...
-  ],
+  declarations: [ProfileTabnavComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule, ProfileTabnavComponent],
 })
 export class ProfileTabnavModule {}
--- src/app/features/projects/projects.module.ts
+++ src/app/features/projects/projects.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './projects.component';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'main', component: ProjectsComponent }];
+const routes: Routes = [{ path: 'projects', component: ProjectsComponent }];
 
 @NgModule({
   declarations: [ProjectsComponent],
