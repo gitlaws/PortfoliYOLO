3458d92834374420e41bff7f1bc921e7cb20b4ee
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-23 13:46:11 -0400

    Fix routing configuration and adjust profile tabnav colors

--- src/app/core/home/home.module.ts
+++ src/app/core/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'Home', component: HomeComponent }];
+const routes: Routes = [{ path: 'home', component: HomeComponent }];
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
