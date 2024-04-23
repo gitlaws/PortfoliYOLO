f6f9991b30e7c9950db4cf81c5fe82982ae87dc9
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-23 18:48:58 -0400

    Refactor routing configuration and adjust profile tabnav colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsComponent } from './features/projects/projects.component';
-import { ProfileTabnavComponent } from './features/profile-tabnav/profile-tabnav.component';
-import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
