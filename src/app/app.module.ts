5bc3daeacadd4b192f0915d0834a396aa15e5e66
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-24 19:26:17 -0400

    Update routing configuration and adjust profile tabnav links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { ToolbarModule } from './core/toolbar/toolbar.module';
 import { AppComponent } from './app.component';
 import { FooterModule } from './core/footer/footer.module';
-import { ToolbarMenuModule } from './core/toolbar-menu/toolbar-menu.module';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ToolbarModule,
-    FooterModule,
-    ToolbarMenuModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, ToolbarModule, FooterModule],
 })
 export class AppModule {}
