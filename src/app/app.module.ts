5356f1867597a0c553c68ec97f5ee7777a357d72
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-04-24 20:01:57 -0400

    Add projects and profile tabnav components to app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { ToolbarModule } from './core/toolbar/toolbar.module';
 import { AppComponent } from './app.component';
 import { FooterModule } from './core/footer/footer.module';
+import { ProjectsModule } from './features/projects/projects.module';
+import { ProfileTabnavModule } from './features/profile-tabnav/profile-tabnav.module';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ToolbarModule, FooterModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ToolbarModule,
+    FooterModule,
+    ProjectsModule,
+    ProfileTabnavModule,
+  ],
 })
 export class AppModule {}
