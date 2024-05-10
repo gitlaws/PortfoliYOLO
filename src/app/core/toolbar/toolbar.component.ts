bebc380f566173a35a6e9844b927767e656a2fa0
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-10 13:48:44 -0400

    Refactor module imports and add SharedModule to toolbar.module.ts, CubeLogoModule, and cyber-menu.module.ts

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CubeLogoComponent } from './cube-logo/cube-logo.component';
+import { CyberMenuComponent } from '../cyber-menu/cyber-menu.component';
+import { ThemeToggleComponent } from '../../shared/common/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
-  imports: [],
+  imports: [CubeLogoComponent, CyberMenuComponent, ThemeToggleComponent],
 })
 export class ToolbarComponent {}
