cccdbc8bf22e2116881edde57bab59f4d50bc182
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-10 13:34:28 -0400

    Refactor module imports and remove unused components in toolbar and projects components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  Router,
-  Event,
-  NavigationStart,
-  NavigationEnd,
-  NavigationError,
-  NavigationCancel,
-} from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'PortfoliYOLO';
-
-  constructor(private router: Router) {
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationStart) {
-        console.log('NavigationStart: ', event);
-      }
-
-      if (event instanceof NavigationEnd) {
-        console.log('NavigationEnd: ', event);
-      }
-
-      if (event instanceof NavigationError) {
-        console.log('NavigationError: ', event);
-      }
-    });
-  }
-
-  ngOnInit() {}
 }
