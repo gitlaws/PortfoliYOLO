75d961d5f67bdb4569da5f77a10f8d58389d68be
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-12 18:38:52 -0400

    Refactor app routes to include lazy loading for projects and profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,3 @@
-// import { Routes } from '@angular/router';
-
-// import { HomeComponent } from './core/home/home.component';
-
-// export const routes: Routes = [
-//   // { path: '', component: HomeComponent },
-//   // {
-//     path: 'projects',
-//     loadComponent: () =>
-//       import('./features/projects/projects.component').then(
-//         (m) => m.ProjectsComponent
-//       ),
-//   },
-//   {
-//     path: 'profile-tabnav',
-//     loadComponent: () =>
-//       import('./features/profile-tabnav/profile-tabnav.component').then(
-//         (m) => m.ProfileTabnavComponent
-//       ),
-//   },
-// ];
-
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
