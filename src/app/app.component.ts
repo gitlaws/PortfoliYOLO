852fa3a014715963da6bc098e8fd86a1b756cada
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-11 23:41:48 -0400

    Refactor app routes to include lazy loading for projects and profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './core/toolbar/toolbar.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ProfileTabnavComponent } from './features/profile-tabnav/profile-tabnav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterOutlet, ToolbarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ToolbarComponent,
+    FooterComponent,
+    ProjectsComponent,
+    ProfileTabnavComponent,
+  ],
 })
 export class AppComponent {
   title = 'PortfoliYOLO';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,41 @@
+// import { Routes } from '@angular/router';
+
+// import { HomeComponent } from './core/home/home.component';
+
+// export const routes: Routes = [
+//   // { path: '', component: HomeComponent },
+//   // {
+//     path: 'projects',
+//     loadComponent: () =>
+//       import('./features/projects/projects.component').then(
+//         (m) => m.ProjectsComponent
+//       ),
+//   },
+//   {
+//     path: 'profile-tabnav',
+//     loadComponent: () =>
+//       import('./features/profile-tabnav/profile-tabnav.component').then(
+//         (m) => m.ProfileTabnavComponent
+//       ),
+//   },
+// ];
+
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './core/home/home.component';
-
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-
   {
     path: 'projects',
     loadComponent: () =>
       import('./features/projects/projects.component').then(
-        (com) => com.ProjectsComponent
+        (m) => m.ProjectsComponent
       ),
   },
+  {
+    path: 'profile-tabnav',
+    loadComponent: () =>
+      import('./features/profile-tabnav/profile-tabnav.component').then(
+        (m) => m.ProfileTabnavComponent
+      ),
+  },
+  // other routes...
 ];
