8ed1d5467d006cd347b789055cbd56bcce5fe85d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-13 21:32:27 -0400

    Refactor app routes to include lazy loading for Home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'Home',
     loadComponent: () =>
       import('./core/home/home.component').then((m) => m.HomeComponent),
   },
