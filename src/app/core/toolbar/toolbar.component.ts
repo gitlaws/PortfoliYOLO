5266cc1fcfd21599ef062b8c6260d26e302d0ea5
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-16 17:36:31 -0400

    Refactor toolbar component to update header styling and add theme support

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 import { CubeLogoComponent } from './cube-logo/cube-logo.component';
 import { CyberMenuComponent } from '../cyber-menu/cyber-menu.component';
 import { ThemeToggleComponent } from '../../shared/common/theme-toggle/theme-toggle.component';
 import { CommonModule } from '@angular/common';
 import { SocialLinksComponent } from '../../shared/common/social-links/social-links.component';
 import { RouterLink } from '@angular/router';
-
-import { Theme } from '../../shared/models/theme.enum';
-import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-toolbar',
