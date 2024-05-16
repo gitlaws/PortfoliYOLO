22c875adb056d8a39c59b95263f794d7c036e15d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-16 17:19:26 -0400

    Refactor toolbar component to implement theme support and update styles

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CubeLogoComponent } from './cube-logo/cube-logo.component';
 import { CyberMenuComponent } from '../cyber-menu/cyber-menu.component';
 import { ThemeToggleComponent } from '../../shared/common/theme-toggle/theme-toggle.component';
 import { CommonModule } from '@angular/common';
 import { SocialLinksComponent } from '../../shared/common/social-links/social-links.component';
 import { RouterLink } from '@angular/router';
+
 import { Theme } from '../../shared/models/theme.enum';
-import { Inject } from '@angular/core';
 import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
@@ -23,7 +23,7 @@ import { ThemeService } from '../../shared/services/theme/theme.service';
     SocialLinksComponent,
   ],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   theme!: Theme;
 
   constructor(private themeService: ThemeService) {}
