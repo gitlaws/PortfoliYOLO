43a0e5ea5d31f80ce823083c0b3da7a5ce17fdb8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-16 17:07:51 -0400

    Refactor toolbar component to update header styling and add theme support

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -5,6 +5,9 @@ import { ThemeToggleComponent } from '../../shared/common/theme-toggle/theme-tog
 import { CommonModule } from '@angular/common';
 import { SocialLinksComponent } from '../../shared/common/social-links/social-links.component';
 import { RouterLink } from '@angular/router';
+import { Theme } from '../../shared/models/theme.enum';
+import { Inject } from '@angular/core';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -21,11 +24,13 @@ import { RouterLink } from '@angular/router';
   ],
 })
 export class ToolbarComponent {
-  // theme: Theme;
-  // constructor(private themeService: ThemeService) { }
-  // ngOnInit() {
-  //   this.themeService.currentTheme.subscribe(theme => {
-  //     this.theme = theme;
-  //   });
-  // }
+  theme!: Theme;
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme;
+    });
+  }
 }
