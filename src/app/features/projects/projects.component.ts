5f4ec75801f74f1481587e4e65fd5d816470a122
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-05-25 15:22:19 -0400

    Refactor projects component to update intro description and improve styling

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-projects',
@@ -8,4 +10,14 @@ import { Component } from '@angular/core';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  theme!: 'light' | 'dark';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
+}
