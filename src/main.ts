c89b97160d180964e498aad6fc4dcefad2a64509
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-07-01 18:09:38 -0400

    Refactor main.ts to set base href from environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+// Set base href from environment
+const baseElement = document.querySelector('base');
+if (baseElement) {
+  baseElement.setAttribute('href', environment.baseHref);
+} else {
+  console.error('Base element not found!');
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
