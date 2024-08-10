ac31b4f3392ac25a5b52b3d271998188fde7d47f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-10 15:08:30 -0400

    Refactor assets path in angular.json for production deployment

---
+++ dist/PortfoliYOLO/browser/chunk-L75DSFX3.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-AW44ZRXI.js";import"./chunk-7XPGD5NU.js";import"./chunk-EUTMQMMX.js";export{a as ProfileTabnavComponent};
---
+++ dist/PortfoliYOLO/browser/chunk-RTJZYEY5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-HUQZ6VDL.js";import"./chunk-X4WJTENS.js";import"./chunk-EUTMQMMX.js";export{a as ProjectsComponent};
