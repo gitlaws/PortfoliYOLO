e73fb2a78d75d286bc687cf90541510e66395a82
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-10 17:38:35 -0400

    Refactor git-grid and profile components for improved performance and layout

---
+++ src/app/core/home/git-grid/git-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-git-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './git-grid.component.html',
+  styleUrl: './git-grid.component.scss'
+})
+export class GitGridComponent {
+
+}
