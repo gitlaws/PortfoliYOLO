809c9a49003192d2f3198d56b8446c3ce81c6afc
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-10 18:06:43 -0400

    Refactor graph component for improved layout and performance

--- src/app/core/home/graph/graph.component.ts
+++ src/app/core/home/graph/graph.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './graph.component.html',
-  styleUrl: './graph.component.scss'
+  styleUrls: ['./graph.component.scss'],
 })
-export class GraphComponent {
+export class GraphComponent implements OnInit {
+  squares: { level: number }[] = [];
 
+  ngOnInit(): void {
+    // Generate squares data
+    for (let i = 1; i < 365; i++) {
+      this.squares.push({ level: Math.floor(Math.random() * 3) });
+    }
+  }
 }
