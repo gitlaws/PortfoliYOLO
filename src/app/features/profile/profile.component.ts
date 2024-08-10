583dc6f4ef5dbd59b385d68debddce767b2bb0d3
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-10 16:09:05 -0400

    Refactor profile component and related files

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  theme!: 'light' | 'dark';
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
 }
