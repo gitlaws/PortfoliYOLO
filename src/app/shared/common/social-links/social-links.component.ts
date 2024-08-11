bc4e1bd3f802b5499b0ab6ddff2156d9baca16d3
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-11 16:57:27 -0400

    Refactor social links component styles for light and dark mode

--- src/app/shared/common/social-links/social-links.component.ts
+++ src/app/shared/common/social-links/social-links.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Theme } from '../../models/theme.enum';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-social-links',
   standalone: true,
+  imports: [CommonModule], // Import CommonModule here
   templateUrl: './social-links.component.html',
-  styleUrl: './social-links.component.scss',
+  styleUrls: ['./social-links.component.scss'],
 })
 export class SocialLinksComponent {
-  isDarkMode: boolean = false;
+  theme!: 'light' | 'dark';
+  isDarkMode: boolean = false; // Define the isDarkMode property
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+      this.isDarkMode = this.theme === 'dark'; // Set isDarkMode based on the current theme
+    });
+  }
 }
