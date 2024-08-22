f85f717eae66530033ea3f37d8759e74a292c6fd
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-22 00:51:38 -0400

    Refactor starfield component imports and remove unused code

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ThemeService } from '../../shared/services/theme/theme.service';
 import { Theme } from '../../shared/models/theme.enum';
-import { GitGridComponent } from './git-grid/git-grid.component';
-import { StarfieldComponent } from './starfield/starfield.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, GitGridComponent, StarfieldComponent],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
