32ad3ff5a00154822c244c12e474cd0c61dda9c6
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-23 18:13:30 -0400

    Refactor app component HTML for improved structure and maintainability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { ToolbarComponent } from './core/toolbar/toolbar.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { ProjectsComponent } from './features/projects/projects.component';
 import { ProfileComponent } from './features/profile/profile.component';
-import { Theme } from './shared/models/theme.enum';
-import { ThemeService } from './shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -24,13 +22,4 @@ import { ThemeService } from './shared/services/theme/theme.service';
 })
 export class AppComponent {
   title = 'PortfoliYOLO';
-  theme!: 'light' | 'dark';
-
-  constructor(private themeService: ThemeService) {}
-
-  ngOnInit(): void {
-    this.themeService.currentTheme.subscribe((theme) => {
-      this.theme = theme === Theme.Light ? 'light' : 'dark';
-    });
-  }
 }
