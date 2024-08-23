30af06944d0f49210896adaf81b6263dd896f68f
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-23 19:11:12 -0400

    chore: Remove GitHub calendar from projects component

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Theme } from '../../shared/models/theme.enum';
 import { ThemeService } from '../../shared/services/theme/theme.service';
 
-declare var GitHubCalendar: any; // Declare GitHubCalendar to avoid TypeScript errors
-
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -21,21 +19,5 @@ export class ProjectsComponent implements OnInit {
     this.themeService.currentTheme.subscribe((theme) => {
       this.theme = theme === Theme.Light ? 'light' : 'dark';
     });
-
-    // Initialize GitHub Calendar
-    // GitHubCalendar('.calendar', 'gitlaws');
-
-    // Enable responsive functionality
-    GitHubCalendar('.calendar', 'gitlaws', { responsive: true });
-
-    // Disable global stats
-    GitHubCalendar('.calendar', 'gitlaws', { global_stats: true });
-
-    // Use a proxy with type assertion
-    GitHubCalendar('.calendar', 'gitlaws', {
-      proxy(gitlaws: string) {
-        return fetch(`https://your-proxy.com/github?user=${gitlaws}`);
-      },
-    } as any).then((r: Response) => r.text());
   }
 }
