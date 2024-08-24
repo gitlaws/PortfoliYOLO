66e4cd3b52bf3683cddb07269d8e8db445f9ab8e
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-24 19:16:32 -0400

    Refactor projects component to use new card layout and update card styling

---
+++ dist/PortfoliYOLO/browser/chunk-ACKVUN5J.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-BQZWPWL4.js";import"./chunk-ZQ43A2FS.js";export{a as ProfileComponent};
--- dist/PortfoliYOLO/browser/chunk-N7GPZN5Q.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-X6S2NQ62.js";import"./chunk-ZGAXCOBT.js";export{a as ProfileComponent};
