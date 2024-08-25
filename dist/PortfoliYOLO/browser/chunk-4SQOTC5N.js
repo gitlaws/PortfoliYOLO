4a0355045665fdcb793529279d22e48697566ec3
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-24 21:57:16 -0400

    Refactor link-tree component to remove unused code and improve styles

---
+++ dist/PortfoliYOLO/browser/chunk-4SQOTC5N.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-QHQE7MIE.js";import"./chunk-ZQ43A2FS.js";export{a as ProfileComponent};
--- dist/PortfoliYOLO/browser/chunk-IBXOMWHR.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-KTBNTOKT.js";import"./chunk-ZQ43A2FS.js";export{a as ProfileComponent};
