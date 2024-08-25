d482494f49aefd68349500ad70cb5537c60db217
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-25 00:56:19 -0400

    Refactor typewriter component to use Typewriter library for animated text effect

--- src/app/core/home/typewriter/typewriter.component.ts
+++ src/app/core/home/typewriter/typewriter.component.ts
@@ -1,11 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core';
 
 @Component({
   selector: 'app-typewriter',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './typewriter.component.html',
-  styleUrl: './typewriter.component.scss',
+  styleUrls: ['./typewriter.component.scss'],
 })
-export class TypewriterComponent {}
+export class TypewriterComponent implements AfterViewInit {
+  app: HTMLElement | null = null;
+
+  ngAfterViewInit() {
+    this.app = document.getElementById('app');
+
+    if (this.app) {
+      const typewriter = new Typewriter(this.app, {
+        loop: true,
+        delay: 75,
+      });
+
+      typewriter
+        .pauseFor(2500)
+        .typeString('A simple yet powerful native javascript')
+        .pauseFor(300)
+        .deleteChars(10)
+        .typeString(
+          '<strong>JS</strong> plugin for a cool typewriter effect and '
+        )
+        .typeString(
+          '<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
+        )
+        .pauseFor(1000)
+        .start();
+    }
+  }
+}
