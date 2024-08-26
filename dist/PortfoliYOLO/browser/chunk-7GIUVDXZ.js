ec0451b49cd1d5e69eb3c718a597391f11b7a141
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-08-26 10:39:35 -0400

    Refactor projects component to update card content and padding

--- dist/PortfoliYOLO/browser/chunk-7GIUVDXZ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-PQRKZ3UN.js";import"./chunk-FSEBZGGM.js";export{a as ProjectsComponent};
---
+++ dist/PortfoliYOLO/browser/chunk-KCGCPFDY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-RRRDYGNX.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
