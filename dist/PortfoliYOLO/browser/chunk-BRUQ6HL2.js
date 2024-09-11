83604505f5624633e5c3a6ecb58b3b6e48c2747c
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-11 17:26:40 -0400

    Refactor profile and projects components

---
+++ dist/PortfoliYOLO/browser/chunk-BRUQ6HL2.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-O7CRL5QN.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
--- dist/PortfoliYOLO/browser/chunk-UJOUAQBJ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-VVUT5JCQ.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
