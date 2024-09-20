3c377871c75447b1596fdeb9b95ea14cb9f60121
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-19 23:06:46 -0400

    Refactor projects component to add Ignite project card and remove unnecessary files

--- dist/PortfoliYOLO/browser/chunk-BRUQ6HL2.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-O7CRL5QN.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
---
+++ dist/PortfoliYOLO/browser/chunk-SRQ3SKZM.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-BKBZ7FIB.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
