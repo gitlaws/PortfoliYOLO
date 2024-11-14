a7ba6c43b3f972a8b37ff953b11e0fed55d9df60
Staś <lawmski@gmail.com>
2024-11-14 16:19:54 -0500

    updated button themes

--- src/app/features/carousel/carousel.component.ts
+++ src/app/features/carousel/carousel.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from '../profile/profile.component';
 import { CompensationComponent } from '../compensation/compensation.component';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-carousel',
@@ -10,10 +12,18 @@ import { CompensationComponent } from '../compensation/compensation.component';
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
   currentIndex = 0;
   components = [ProfileComponent, CompensationComponent];
-  theme = 'light'; // or 'dark', based on your theme logic
+  theme!: 'light' | 'dark';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
 
   nextPage() {
     this.currentIndex = (this.currentIndex + 1) % this.components.length;
