1dd1a71e80c33e3e0d0c680060bc82c90d4ccb7b
Staś <lawmski@gmail.com>
2024-11-14 16:11:10 -0500

    updates

--- src/app/features/carousel/carousel.component.ts
+++ src/app/features/carousel/carousel.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from '../profile/profile.component';
-import { Theme } from '../../shared/models/theme.enum';
-import { ThemeService } from '../../shared/services/theme/theme.service';
 import { CompensationComponent } from '../compensation/compensation.component';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [CommonModule, ProfileComponent],
+  imports: [CommonModule, ProfileComponent, CompensationComponent],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselComponent {
   currentIndex = 0;
   components = [ProfileComponent, CompensationComponent];
+  theme = 'light'; // or 'dark', based on your theme logic
 
   nextPage() {
     this.currentIndex = (this.currentIndex + 1) % this.components.length;
@@ -23,15 +22,5 @@ export class CarouselComponent {
   prevPage() {
     this.currentIndex =
       (this.currentIndex - 1 + this.components.length) % this.components.length;
-  }
-
-  theme!: 'light' | 'dark';
-
-  constructor(private themeService: ThemeService) {}
-
-  ngOnInit(): void {
-    this.themeService.currentTheme.subscribe((theme) => {
-      this.theme = theme === Theme.Light ? 'light' : 'dark';
-    });
   }
 }
