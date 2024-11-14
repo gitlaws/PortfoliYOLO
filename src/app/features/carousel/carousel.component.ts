cc7c40a9b80035d25db3d4cc4414d0d93e1e5cd1
Staś <lawmski@gmail.com>
2024-11-14 16:03:55 -0500

    update

--- src/app/features/carousel/carousel.component.ts
+++ src/app/features/carousel/carousel.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from '../profile/profile.component';
 import { Theme } from '../../shared/models/theme.enum';
 import { ThemeService } from '../../shared/services/theme/theme.service';
+import { CompensationComponent } from '../compensation/compensation.component';
 
 @Component({
   selector: 'app-carousel',
@@ -13,7 +14,7 @@ import { ThemeService } from '../../shared/services/theme/theme.service';
 })
 export class CarouselComponent {
   currentIndex = 0;
-  components = [ProfileComponent];
+  components = [ProfileComponent, CompensationComponent];
 
   nextPage() {
     this.currentIndex = (this.currentIndex + 1) % this.components.length;
