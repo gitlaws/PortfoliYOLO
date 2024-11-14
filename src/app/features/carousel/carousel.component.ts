6f38f948ef66bf663614f90e969e3d53ec7e14dc
Staś <lawmski@gmail.com>
2024-11-14 15:59:30 -0500

    update theme to carasoul buttons

--- src/app/features/carousel/carousel.component.ts
+++ src/app/features/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from '../profile/profile.component';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-carousel',
@@ -20,5 +22,15 @@ export class CarouselComponent {
   prevPage() {
     this.currentIndex =
       (this.currentIndex - 1 + this.components.length) % this.components.length;
+  }
+
+  theme!: 'light' | 'dark';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
   }
 }
