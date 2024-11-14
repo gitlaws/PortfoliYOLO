63ba76cd56f6cb6a6271e61d573dfacc14aa4f19
Staś <lawmski@gmail.com>
2024-11-14 17:01:45 -0500

    updates

--- src/app/features/compensation/compensation.component.ts
+++ src/app/features/compensation/compensation.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-compensation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './compensation.component.html',
-  styleUrl: './compensation.component.scss'
+  styleUrl: './compensation.component.scss',
 })
 export class CompensationComponent {
+  theme!: 'light' | 'dark';
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
 }
