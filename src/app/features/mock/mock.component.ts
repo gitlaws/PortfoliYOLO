cb1a0c1c0135431d914be6adcf2888614c559173
Staś <lawmski@gmail.com>
2024-11-27 19:01:16 -0500

    update dark themes to card footer

--- src/app/features/mock/mock.component.ts
+++ src/app/features/mock/mock.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-mock',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mock.component.html',
-  styleUrl: './mock.component.scss'
+  styleUrl: './mock.component.scss',
 })
 export class MockComponent {
+  theme!: 'light' | 'dark';
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
 }
