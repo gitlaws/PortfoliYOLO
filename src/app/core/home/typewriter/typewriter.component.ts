5371214d665cbe0bc18135ce04a7234c952ecf42
Staś <lawmski@gmail.com>
2024-12-12 19:51:08 -0500

    update

--- src/app/core/home/typewriter/typewriter.component.ts
+++ src/app/core/home/typewriter/typewriter.component.ts
@@ -31,6 +31,7 @@ export class TypewriterComponent implements AfterViewInit, OnInit {
       'Code refactoring',
       'Responsive design',
       'Cross-browser compatibility',
+      'Security',
     ];
 
     const container = this.elRef.nativeElement.querySelector(
