faafbd53ba53933772af718c2aa480cc95bf2006
Staś <lawmski@gmail.com>
2024-12-15 04:54:53 -0500

    Auto-generated commit [ci skip]

---
+++ chunk-4PYO75QY.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-XFJEVMSL.js";import"./chunk-VA27ORFX.js";export{a as ProjectsComponent};
---
+++ chunk-WUQG5TAD.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-UGMMQ5I4.js";import"./chunk-VA27ORFX.js";export{a as ProfileComponent};
