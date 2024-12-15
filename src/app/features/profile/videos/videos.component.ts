3e497d5f240091b2948b066dd3913976d5bcac3e
Staś <lawmski@gmail.com>
2024-12-15 03:34:36 -0500

    updates

--- src/app/features/profile/videos/videos.component.ts
+++ src/app/features/profile/videos/videos.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Theme } from '../../../shared/models/theme.enum';
+import { ThemeService } from '../../../shared/services/theme/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './videos.component.html',
-  styleUrl: './videos.component.scss'
+  styleUrl: './videos.component.scss',
 })
 export class VideosComponent {
+  theme!: 'light' | 'dark';
 
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
 }
