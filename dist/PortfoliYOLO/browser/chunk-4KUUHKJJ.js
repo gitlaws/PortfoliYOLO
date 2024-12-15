d17ffff5cc7f1a34b828e4a228b2822f01f6d72a
Staś <lawmski@gmail.com>
2024-12-15 04:55:06 -0500

    updated run deploy

--- dist/PortfoliYOLO/browser/chunk-4KUUHKJJ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-GMMAGDA7.js";import"./chunk-IK6CA5DJ.js";export{a as ProjectsComponent};
--- dist/PortfoliYOLO/browser/chunk-K2THPVGS.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-YEJ5XBJI.js";import"./chunk-IK6CA5DJ.js";export{a as ProfileComponent};
