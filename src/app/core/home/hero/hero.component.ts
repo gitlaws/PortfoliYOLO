d39ec4dec5906f8198dbe8e4228870617b978b6d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-07-29 21:06:05 -0400

    update

--- src/app/core/home/hero/hero.component.ts
+++ src/app/core/home/hero/hero.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { ThemeService } from '../../../shared/services/theme/theme.service';
 import { Theme } from '../../../shared/models/theme.enum';
 import { CommonModule } from '@angular/common';
+import { TypewriterComponent } from '../typewriter/typewriter.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TypewriterComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  title: string;
+  theme!: 'light' | 'dark';
+}
