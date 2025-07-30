96cb4ce34ab529a92facd9fe9a63adec8bf87840
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-07-29 23:24:34 -0400

    Auto-generated commit [ci skip]

--- chunk-KDPQA5VT.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-J4LK5ME2.js";import"./chunk-CQYF4DBW.js";import"./chunk-EQDQRRRY.js";export{a as ProjectsComponent};
---
+++ chunk-TOGB3MHV.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-EZB5BH5M.js";import"./chunk-5QBUDGXN.js";import"./chunk-EQDQRRRY.js";export{a as ProfileComponent};
