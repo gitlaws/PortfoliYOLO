3b9cbe10ed5ada69e2414693c68fc9d198043d26
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-08-08 23:28:31 -0400

    update

--- src/app/features/projx/projx.component.ts
+++ src/app/features/projx/projx.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Theme } from '../../shared/models/theme.enum';
+import { ThemeService } from '../../shared/services/theme/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projx',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projx.component.html',
   styleUrl: './projx.component.scss',
 })
-export class ProjxComponent {}
+export class ProjxComponent implements OnInit {
+  theme!: 'light' | 'dark';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.themeService.currentTheme.subscribe((theme) => {
+      this.theme = theme === Theme.Light ? 'light' : 'dark';
+    });
+  }
+}
