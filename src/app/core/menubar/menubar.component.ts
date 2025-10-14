9ca4b8f2541de3fcf6b0b44ddad8b4358d6e1b9a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-10-13 20:46:39 -0400

    update

--- src/app/core/menubar/menubar.component.ts
+++ src/app/core/menubar/menubar.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menubar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menubar.component.html',
-  styleUrl: './menubar.component.scss',
+  styleUrls: ['./menubar.component.scss'],
 })
 export class MenubarComponent {
   isMenuOpen = false;
+  theme: 'theme-light' | 'theme-dark' = 'theme-light';
+
+  constructor() {
+    if (
+      typeof window !== 'undefined' &&
+      window.matchMedia?.('(prefers-color-scheme: dark)')
+    ) {
+      this.theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'theme-dark'
+        : 'theme-light';
+    }
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
@@ -16,5 +29,9 @@ export class MenubarComponent {
 
   closeMenu(): void {
     this.isMenuOpen = false;
+  }
+
+  toggleTheme(): void {
+    this.theme = this.theme === 'theme-dark' ? 'theme-light' : 'theme-dark';
   }
 }
