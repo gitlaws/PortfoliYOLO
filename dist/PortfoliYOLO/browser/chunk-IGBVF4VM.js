f8887a328fdb8b721b0e01c1cba4c56f16d7051a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2025-10-14 21:48:33 -0400

    update

--- dist/PortfoliYOLO/browser/chunk-IGBVF4VM.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-BYK7WZG4.js";import"./chunk-XYCTIM3K.js";export{a as ProfileComponent};
---
+++ dist/PortfoliYOLO/browser/chunk-YOO4TSYP.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-4UALBTVP.js";import"./chunk-XYCTIM3K.js";export{a as ProfileComponent};
